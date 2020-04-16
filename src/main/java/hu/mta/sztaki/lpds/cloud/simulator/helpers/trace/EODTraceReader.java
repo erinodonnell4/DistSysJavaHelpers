74acc4c06172b2c4564c78ce16af3436bd7c135c
Erin O'Donell <erinodonnell4@hotmail.com>
2020-04-16 16:48:05 +0100

    added

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/trace/EODTraceReader.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/trace/EODTraceReader.java
@@ -1,98 +1,125 @@
-package hu.mta.sztaki.lpds.cloud.simulator.helpers.trace;
+package hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.file;
 
-import java.lang.reflect.InvocationTargetException;
 import hu.mta.sztaki.lpds.cloud.simulator.helpers.job.Job;
 import hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.file.TraceFileReaderFoundation;
 
+
 public class EODTraceReader extends TraceFileReaderFoundation {   
 
-	TraceFileReaderFoundation TFRF;
-	
-	//NEEDS FIXED
+
+
+	private String line;
+
 
 	public EODTraceReader(String fileName, int from, int to, boolean furtherjobs, Class<? extends Job> jobType)
-			throws SecurityException, NoSuchMethodException {
-		super("Grid workload format", fileName, from, to, furtherjobs, jobType);
-		// TODO Auto-generated constructor stub
+	
+		throws SecurityException, NoSuchMethodException {
+		super("log Format", fileName, from, to, furtherjobs, jobType);
 	}
-
-	protected boolean isTraceLine(final String param) {
-		boolean isValid = false;
-		String[] test = param.split(" ");
-
-		if (test[0].equals(test[0].valueOf(0))) {
-			if (test[1].equals(test[1].valueOf(1))) {
-				if (test[3].equalsIgnoreCase("default")) {
-					isValid = true;
-				} else if (test[3].equalsIgnoreCase("url")) {
-					isValid = true;
-				} else if (test[3].equalsIgnoreCase("export")) {
-					isValid = true;
-
-				}
-			}
-		}
-
-		if (isValid = true) {
-			return true;
-		} else
-			return false;
-
-	}
-
-	protected void metaDataCollector(String data) {
-
-	}
+	
+	//Create Job
 
 	protected Job createJobFromLine(String job)
+	
 			throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
-		final String[] fragments = job.trim().split("\\s+");
+		line = null;
+		final String[] lineData = line.split(" "); 
 		try {
-			/**
-			 * String id, long submit, long queue, long exec, int nprocs, double ppCpu, long
-			 * ppMem, String user, String group, String executable, Job preceding, long
-			 * delayAfter
-			 */
-			// 1 done, 0 fail, 5 cancel
-			int jobState = Integer.parseInt(fragments[10]);
-			int procs = Integer.parseInt(fragments[4]);
-			long runtime = Long.parseLong(fragments[3]);
-			long waitTime = Long.parseLong(fragments[2]);
-			if (jobState != 1 && (procs < 1 || runtime < 0)) {
+			
+			
+			int jobState = Integer.parseInt(lineData[0]);   //job id string
+			int procs = 1; 								   // allocated processors
+			long submit = Long.parseLong(lineData[1]);    // submit time in seconds
+			long waitTime = 0; 							 // waiting time in seconds
+			long runTime = Long.parseLong(lineData[3]); // running time in seconds
+			String user = null; 					   // userID
+			String group = null;                      // groupID
+			String exec = null;                      // execID
+			if (jobState != 1 && (procs < 1 || runTime < 0)) {
 				return null;
+				
 			} else {
-				final String preceedingJobId = fragments[16].trim();
-				Job preceedingJob = null;
+				
+				final String preceedingJobId = lineData[1].trim();
 				if (!preceedingJobId.equals("-1")) {
-					preceedingJob = jobLookupInCache(preceedingJobId);
+					return jobCreator.newInstance(jobState, procs, submit, waitTime, runTime, user, group, exec); 
 				}
-				return jobCreator.newInstance(
-						// id:
-						fragments[0],
-						// submit time in secs:
-						Long.parseLong(fragments[1]),
-						// wait time in secs:
-						Math.max(0, waitTime),
-						// run time in secs:
-						Math.max(0, runtime),
-						// allocated processors:
-						Math.max(1, procs),
-						// average cpu time:
-						(long) Double.parseDouble(fragments[5]),
-						// average memory:
-						Long.parseLong(fragments[6]),
-						// userid:
-						fragments[11],
-						// groupid:
-						fragments[12],
-						// execid:
-						fragments[13], preceedingJob, preceedingJob == null ? 0 : Long.parseLong(fragments[17]));
+				
+				
+				}
+			
+		}
+				catch (ArrayIndexOutOfBoundsException e) {
+					// Line is null
+				return null;
+				}
+		return null;
+				
+		}
+			
+			
+			protected void metaDataCollector(String data) {
+			
 			}
 
-		} //catch (ArrayIndexOutOfBoundsException ex) {
-			// Ignore
-		}
-		return null;
-	}
-}
+			
+			protected boolean isTraceLine(String line) throws ArrayIndexOutOfBoundsException {
+				
+				String[] lineData; // Instantiate string array.
+				
+				// Check if the line argument is null.
+				if(line != null) {
+					// Not null, add its contents to the array.
+					lineData = line.split(" ");
+				} else {
+					// Line is null, invalid.
+					return false;
+				}
+				
+				// Catch invalid lines with ArrayIndexOutOfBoundsException
+				try {
+					// Check for Job Arrival Time Integer
+					try {
+						Integer.parseInt(lineData[0]);
+					} catch (NumberFormatException e) {
+						// Data can't be parsed as an integer, data line contains incorrect data.
+						return false;
+					}
+					
+					// Check for Job Duration Float
+					try {
+						Float.parseFloat(lineData[1]);
+					} catch (NumberFormatException e) {
+						// Data can't be parsed as a float, data line contains incorrect data.
+						return false;
+					}
+					
+					// Check for Job ID String ( Not Whitespace )
+					if(lineData[2].isEmpty()) {
+						
+						return false;
+					}
+					
+					// Check for Job Executable String ( "url", "default" or "export" )
+					if(!lineData[3].isEmpty()) {
+						// Not blank
+						if(!lineData[3].equals("url") && !lineData[3].equals("default") && !lineData[3].equals("export")) {
+							// Doesn't equal any of the specified job executable types
+							return false;
+						}
+					} else {
+						
+						return false;
+					}
+					
+					// Passed all of the checks, return true.
+					return true;
+					
+				} catch(ArrayIndexOutOfBoundsException e) {
+					// Incorrect amount of arguments.
+					return false;
+				}
+				
+			}
+	
